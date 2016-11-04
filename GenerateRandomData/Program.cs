c9d5eb1ef9ec50c63535b28ee2fb647c96c2da16
David Colwell <d.colwell@tricentis.com>
2016-11-04 12:15:09 +1100

    Add project files.

---
+++ GenerateRandomData/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenerateRandomData
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
